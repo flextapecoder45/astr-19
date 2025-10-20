47658441c9a88a9a8b732875743868fc9d3f2068
flextapecoder45 <dflexoeggs@gmail.com>
2025-10-19 19:54:35 -0700

    Added Session 1 Code for Code Journal
    aslkdf

---
+++ session-1.py
@@ -0,0 +1,10 @@
+# session1.py
+
+def main():
+    print("Full Name: Daniel Flexo")
+    print("Pronouns: he/him")
+    print("My favorite movie is 'Interstellar' because I love Matthew McConaughey.")
+    print("My favorite food is pizza because I couldn't live without it.")
+
+if __name__ == "__main__":
+    main()
